f53aa36880ec6d494ea6f4ae9bfb926710cb843f
Anton Babko <anton.babko@ukma.edu.ua>
2022-10-20 15:01:41 +0300

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by admin on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
